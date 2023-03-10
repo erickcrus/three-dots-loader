6504a604951c05d692e9c8d0a087308463baf9d7
Erick Crus <35431617+erickcrus@users.noreply.github.com>
2023-03-10 11:31:40 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const index = () => {
+    return <View>
+        <Text>TESTE DE PACOTE</Text>
+    </View>
+}
+
+export default index;
