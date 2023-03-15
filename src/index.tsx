36d4e1b0bb81e048b19d935c31b4db271f168fdf
Erick Crus <35431617+erickcrus@users.noreply.github.com>
2023-03-15 13:24:40 -0300

    feat: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ const Loader = (props: Props) => {
     for (let i = 0; i < count; i++) {
       if (sizeList.current) {
         let d = animationDuration ?? 600;
-        sizeList.current[i].value = withRepeat(withDelay(d * i / 2, withSequence(withTiming(1.3, { duration: d }), withTiming(1, { duration: d }))), -1, true);
+        sizeList.current[i].value = withRepeat(withDelay(d * i / 2, withSequence(withTiming(1.3, { duration: d }), withTiming(1, { duration: d }))), -1, true) ?? 1;
       }
     }
   }, [sizeList]);
