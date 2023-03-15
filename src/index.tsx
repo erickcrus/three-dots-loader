be7b10c0901861ddd5d0435d1f7a7ee82a5d9528
Erick Crus <35431617+erickcrus@users.noreply.github.com>
2023-03-15 11:36:24 -0300

    feat: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,52 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { useEffect, useRef } from 'react';
+import { View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
+
+type Props = {
+  size: number,
+  duration?: number,
+  color?: string,
+  spacing?: number,
+  animationDuration?: number
 }
+
+const Loader = (props: Props) => {
+  const { size, animationDuration, color, spacing } = props;
+  const sizeList = useRef([useSharedValue(1), useSharedValue(1), useSharedValue(1)]);
+  const count = sizeList.current.length;
+
+  const circleColor = color ?? '#00b9fc';
+
+  useEffect(() => {
+    for (let i = 0; i < count; i++) {
+      let d = animationDuration ?? 600;
+      sizeList.current[i].value = withRepeat(withDelay(d * i / 2, withSequence(withTiming(1.3, { duration: d }), withTiming(1, { duration: d }))), -1, true);
+    }
+  }, []);
+
+  const animated = useRef([]);
+  let listItems = [];
+  for (let i = 0; i < count; i++) {
+    animated.current[i] = useAnimatedStyle(() => ({
+      transform: [{ scale: sizeList.current[i].value }]
+    }));
+    listItems[i] = <Animated.View key={'ball' + i} style={[{
+      width: size,
+      height: size,
+      marginHorizontal: spacing ?? 3,
+      borderRadius: size / 2,
+      backgroundColor: circleColor,
+    }, animated.current[i]]} />
+  }
+
+  return <View style={{
+    flexGrow: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }}>
+    {listItems}
+  </View>
+}
+
+export default Loader;
